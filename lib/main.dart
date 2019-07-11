6273c3dacbec6d0876d8dd28315c28f6444e77ce
Jose Pedron Tessele <josetesselep@gmail.com>
2019-07-11 17:37:49 -0300

    Getting data from default city usgin the api and displaying on scrren (temperature and city name)

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,19 @@ import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/weather?format=json&key=4f963d06";
 
-void main() async{
+void main() async {
   http.Response response = await http.get(request);
-  print(json.decode(response.body));
-  runApp(MaterialApp(home: MyApp()));
+  print(response.body);
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -32,8 +33,130 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final color = const Color(0xffFDF6E6);
+  String _search;
+
+  Future<Map> _getData() async {
+    http.Response response;
+
+    if (_search == null) {
+      response = await http.get(
+          'https://api.hgbrasil.com/weather?key=4f963d06&city_name=Cacequi,RS');
+      print('sdsdas');
+    } else {
+      http.Response response = await http.get(
+          "https://api.hgbrasil.com/weather?key=4f963d06&city_name=$_search");
+      print('1');
+    }
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getData().then((map) {
+      print(map);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: color,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: InkWell(
+          onTap: () {},
+          child: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 30.0, top: 20.0),
+            child: Text(
+              '°C',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17.0),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: 'Search for a city',
+                  labelStyle: TextStyle(color: Colors.black),
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  )),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getData(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return Stack(
+                        children: <Widget>[
+                          Positioned(
+                            height: 140.0,
+                            left: 135.0,
+                            child: Center(
+                                child: Text(
+                              snapshot.data["results"]["city"]
+                                  .toString()
+                                  .substring(
+                                      0,
+                                      snapshot.data["results"]["city"].length -
+                                          1),
+                              style: TextStyle(
+                                  fontSize: 25.0, fontWeight: FontWeight.bold),
+                            )),
+                          ),
+                          Positioned(
+                            height: 250.0,
+                            left: 140.0,
+                            child: Center(
+                                child: Text(
+                              snapshot.data["results"]["temp"].toString() + '°',
+                              style: TextStyle(
+                                  fontSize: 65.0, fontWeight: FontWeight.bold),
+                            )),
+                          ),
+                        ],
+                      );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
