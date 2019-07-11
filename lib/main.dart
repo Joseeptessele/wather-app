3c30c2775c9b1ccc6a7a294c04cb418fb51c13f6
Jose Pedron Tessele <josetesselep@gmail.com>
2019-07-11 13:58:08 -0300

    getting data from api

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-const request = "https://api.hgbrasil.com/wather?format=json&key=4f963d06";
-void main() => runApp(MyApp());
+const request = "https://api.hgbrasil.com/weather?format=json&key=4f963d06";
+
+void main() async{
+  http.Response response = await http.get(request);
+  print(json.decode(response.body));
+  runApp(MaterialApp(home: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -20,17 +27,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Scaffold();
   }
 }
